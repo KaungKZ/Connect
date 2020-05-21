48f0ede1eece797fc0d3b566fb1b0fc90f4ffcf1
Kaung Khant Zaw <kaungkz93@gmail.com>
2020-05-22 01:37:07 +0630

    finish home m-q

--- src/pages/homePage/components/Header.jsx
+++ src/pages/homePage/components/Header.jsx
@@ -43,7 +43,19 @@ export default function Header() {
     <header className={`home-header ${theme ? "light-mode" : ""}`}>
       <div className="container">
         <div className="logo">
-          <img src={logo} alt="" />
+          {/* <img src={logo} alt="" /> */}
+          <svg
+            width="63"
+            height="17"
+            viewBox="0 0 63 17"
+            fill="none"
+            className="logo-icon"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="8.5" cy="8.5" r="8.5" fill="white" />
+            <circle cx="31" cy="8" r="5" fill="white" />
+            <circle cx="54.5" cy="8.5" r="8.5" fill="white" />
+          </svg>
         </div>
 
         {/* <div className="theme-toggle">
